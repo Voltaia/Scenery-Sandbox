b41393ee9f0b8d915bc17772310e544c57eaab67
Harper Rhett <harperrhett@gmail.com>
2022-11-17 21:15:16 -0600

    New flora placement method

--- Assets/Scripts/FloraGenerator.cs
+++ Assets/Scripts/FloraGenerator.cs
@@ -6,13 +6,14 @@ using UnityEngine;
 // Generates flora on a voxel grid
 public class FloraGenerator
 {
-    // Class variables
-    private VoxelGrid voxelGrid;
+	// Class variables
+	private VoxelGrid voxelGrid;
+	private List<Placement> placements = new List<Placement>();
 
 	// Class settings
-	private const int Spacing = 5;
-	private const int MinimumFlora = 8;
-	private const int MaximumFlora = 12;
+	private const float TreeSeparation = 1;
+	private const int MinimumFailures = 10;
+	private const float SearchFailureMultiplier = 2.5f;
 
 	// Constructor
 	public FloraGenerator(VoxelGrid voxelGrid)
@@ -27,54 +28,93 @@ public class FloraGenerator
 		// Set seed
 		Random.InitState(seed);
 
-		// Get random amount of flora
-		int floraToPlace = Random.Range(MinimumFlora, MaximumFlora);
+		// Reset placements
+		placements.Clear();
 
-		// Loop through placements of flora
-		int floraPlaced = 0;
-		List<Vector3Int> spawnPositions = new List<Vector3Int>();
-		while (floraPlaced < floraToPlace)
+		// Find a spot
+		VoxelGrid voxelGridToPlace = VoxelStructures.GetStructure(StructureType.Tree);
+		float gridStructureRadius = Mathf.Sqrt(Mathf.Pow(voxelGridToPlace.width, 2) + Mathf.Pow(voxelGridToPlace.length, 2)) / 2.0f;
+		float gridPlacementRadius = gridStructureRadius + TreeSeparation;
+		List<Vector3Int> placementPositions = GetPlacementPositions(voxelGrid.width / 2, voxelGrid.length / 2, 64, gridPlacementRadius);
+
+		// Place the flora
+		foreach (Vector3Int placementPosition in placementPositions)
+		{
+			// Place it
+			voxelGrid.WriteVoxelGrid(
+				voxelGridToPlace,
+				placementPosition.x - (voxelGridToPlace.width / 2),
+				placementPosition.y,
+				placementPosition.z - (voxelGridToPlace.length / 2),
+				false
+			);
+		}
+	}
+
+	// Get spots distributed in radius
+	private List<Vector3Int> GetPlacementPositions(int centerX, int centerZ, float searchRadius, float separationRadius)
+	{
+		// Look for spots to place
+		List<Vector3Int> openings = new List<Vector3Int>();
+		int failures = 0;
+		while (failures - MinimumFailures < openings.Count * SearchFailureMultiplier)
 		{
 			// Get random position
-			int spawnX = Random.Range(0, voxelGrid.width);
-			int spawnY = voxelGrid.height;
-			int spawnZ = Random.Range(0, voxelGrid.length);
+			Vector3Int openPosition = new Vector3Int();
+			do
+			{
+				openPosition.x = (int)(centerX + Mathf.Cos(Random.value * Mathf.PI * 2) * Random.value * searchRadius);
+				openPosition.z = (int)(centerZ + Mathf.Sin(Random.value * Mathf.PI * 2) * Random.value * searchRadius);
+			} while (voxelGrid.IsOutOfBounds(openPosition.x, 0, openPosition.z));
 
 			// Check if reasonable distance from other spawn positions
-			bool positionAcceptable = true;
-			foreach (Vector3Int spawnPosition in spawnPositions)
+			bool failedDistanceCheck = false;
+			foreach (Placement placement in placements)
 			{
-				if(VoxelGrid.GetVoxelDistance(
-					spawnX, -1, spawnZ,
-					spawnPosition.x, -1, spawnPosition.z
-				) <= Spacing) positionAcceptable = false;
+				if (VoxelGrid.GetVoxelDistance(
+					openPosition.x, -1, openPosition.z,
+					placement.position.x, -1, placement.position.z
+				) <= Mathf.Ceil(separationRadius)) failedDistanceCheck = true;
+			}
+			if (failedDistanceCheck)
+			{
+				// Failed distance check
+				failures++;
+				continue;
 			}
 
-			// Check if passed the first test
-			if (positionAcceptable)
+			// Check if surface below is acceptable
+			openPosition.y = voxelGrid.GetSurfaceY(openPosition.x, openPosition.z);
+			VoxelType surfaceVoxelType = voxelGrid.ReadVoxel(openPosition.x, openPosition.y, openPosition.z);
+			if (surfaceVoxelType != VoxelType.DripGrass && surfaceVoxelType != VoxelType.Grass)
 			{
-				// Check if surface below is acceptable
-				spawnY = voxelGrid.GetSurfaceY(spawnX, spawnZ);
-				VoxelType surfaceVoxelType = voxelGrid.ReadVoxel(spawnX, spawnY, spawnZ);
-				if (surfaceVoxelType != VoxelType.DripGrass && surfaceVoxelType != VoxelType.Grass) positionAcceptable = false;
-				else spawnY++;
+				// Failed surface check
+				failures++;
+				continue;
 			}
+			else openPosition.y++;
 
-			// Place if acceptable
-			if (positionAcceptable)
-			{
-				// Place it
-				VoxelGrid voxelGridToPlace = VoxelStructures.GetStructure(StructureType.Tree);
-				voxelGrid.WriteVoxelGrid(
-					voxelGridToPlace,
-					spawnX - (voxelGridToPlace.width / 2),
-					spawnY,
-					spawnZ - (voxelGridToPlace.length / 2),
-					false
-				);
-				spawnPositions.Add(new Vector3Int(spawnX, spawnY, spawnZ));
-				floraPlaced++;
-			}
+			// Passed all checks, add position
+			openings.Add(openPosition);
+			placements.Add(new Placement(openPosition, separationRadius));
+		}
+
+		// Return open positions
+		return openings;
+	}
+
+	// Placements
+	private struct Placement
+	{
+		// Struct variables
+		public Vector3Int position;
+		public float radius;
+
+		// Constructor
+		public Placement(Vector3Int position, float radius)
+		{
+			this.position = position;
+			this.radius = radius;
 		}
 	}
 }

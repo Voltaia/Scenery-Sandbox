fdd7c65b9ae52d579e5e14a9d566ea7b22011288
Harper Rhett <harperrhett@gmail.com>
2023-01-14 16:05:39 -0600

    Fixed scaled terrain

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -30,15 +30,20 @@ public class TerrainGenerator
 	// Generate a new terrain
 	public void WriteTerrain(int seed)
 	{
+		// Set seed
+		Random.InitState(seed);
+		float perlinStartX = Random.Range(0, 1000);
+		float perlinStartY = Random.Range(0, 1000);
+
 		// Loop through X and Z coordinates
 		for (int x = 0; x < voxelGrid.width; x++)
 		{
 			for (int z = 0; z < voxelGrid.length; z++)
 			{
 				// Generate height using perlin noise
-				float perlinX = ((float)x / voxelGrid.width) * (seed / 1000.0f);
-				float perlinY = ((float)z / voxelGrid.length) * (seed / 1000.0f);
-				int y = SurfaceStartHeight + (int)(Mathf.PerlinNoise(perlinX, perlinY) * SurfaceVariation * 2);
+				float scaledX = ((float)x / voxelGrid.width);
+				float scaledZ = ((float)z / voxelGrid.length);
+				int y = SurfaceStartHeight + (int)(Mathf.PerlinNoise(perlinStartX + scaledX, perlinStartY + scaledZ) * SurfaceVariation * 2);
 				voxelGrid.WriteVoxel(x, y, z, new Voxel(layers[0].voxelType));
 
 				// Set some things up for depth
